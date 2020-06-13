9d11949ffa6bf1c559b5ebaa68b114006212e5e9
Shivam Sareen <shivamkrsarin1996@gmail.com>
2020-06-13 16:19:36 -0500

    modified run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="shivam/machine-learning"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run machine-learning --generator=run-pod/v1 --image=$dockerpath --port=80 --labels app=flask-app
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward machine-learning 8000:80
 

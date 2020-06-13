75962219b0d41f88da126fbf745a9c2304f62a83
Shivam Sareen <shivamkrsarin1996@gmail.com>
2020-06-13 16:29:22 -0500

    updated upload_docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="shivam/machine-learning"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login 
+docker tag machine-learning $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push shivam/machine-learning
